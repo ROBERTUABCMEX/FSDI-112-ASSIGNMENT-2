a4a87b9a9ae886121f52a6abca2af3acce345c5f
ROBERT VALDES <69652470+ROBERTUABCMEX@users.noreply.github.com>
2020-12-07 11:40:05 -0800

    FSDI 112 ASSIGNMENT 2

---
+++ djangoapp/pages/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import HomePageView, AboutPageView 
+from django.conf import settings
+from django.conf.urls.static import static
+ 
+# Create your views here.
+
+urlpatterns=[
+    path('', HomePageView.as_view(), name="home"),
+    path('about/', AboutPageView.as_view(), name="about"),
+
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
